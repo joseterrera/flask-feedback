1c938133d54d3e7ccbce4a891260b4724d7d5368
Jose Terrera <joseterrera@yahoo.com>
2020-08-09 19:08:45 -0700

    working on models

---
+++ models.py
@@ -0,0 +1,62 @@
+"""Models for flask feedback"""
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+def connect_db(app):
+  """Connect this database to Flask app.
+  Call this in your flask app
+  """
+
+  db.app = app
+  db.init_app(app)
+
+
+class User(db.Model):
+  """Site user"""
+
+  __tablename__ = "users"
+
+  username = db.Column(
+    db.String(20), nullable=False, unique=True, primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False) 
+    last_name = db.Column(db.String(30), nullable=False)
+    feedback = db.relationship('Feedback', backref="user", cascade="all, delete")
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+      """Register a user, hashing their password"""
+      hashed = bcrypt.generate_password_hash(password)
+      hashed_udf8 = hashed.decode('utf8')
+      user = cls(
+        username=username,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+        email=email
+      )
+
+      db.session.add(user)
+      return user
+
+      @classmethod
+      def authenticate(cls, username, password):
+        """Validate that user exists and paswword is correct.
+        Return user if valid, else return False. """
+
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+          return user
+        else:
+          return False
+
+class Feedback(db.Model):
+  """Feedback"""
+  __tablename__ = 'feedback'
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column
