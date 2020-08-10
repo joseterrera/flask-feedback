e2c1e7c8225a6aa8e733723ba80a1c1a1a307272
Jose Terrera <joseterrera@yahoo.com>
2020-08-09 21:37:22 -0700

    finished models, added a base template

--- models.py
+++ models.py
@@ -59,4 +59,6 @@ class Feedback(db.Model):
   __tablename__ = 'feedback'
 
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column
+  title = db.Column(db.String(100), nullable=False)
+  content = db.Column(db.Text, nullable=False)
+  username = db.Column(db.String(20), db.ForeignKey('users.username'), nullable=False)
